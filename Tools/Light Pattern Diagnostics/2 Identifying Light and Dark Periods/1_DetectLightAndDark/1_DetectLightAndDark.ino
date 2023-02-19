3c8d03ad230c848484d27c9d58a7b8c3a1ba01fb
Dr. Tobias Weltner <tobias@powershell.one>
2023-02-19 17:21:20 +0100

    Update 1_DetectLightAndDark.ino

--- Tools/Light Pattern Diagnostics/2 Identifying Light and Dark Periods/1_DetectLightAndDark/1_DetectLightAndDark.ino
+++ Tools/Light Pattern Diagnostics/2 Identifying Light and Dark Periods/1_DetectLightAndDark/1_DetectLightAndDark.ino
@@ -2,19 +2,19 @@
 #define sensor A2
 
 int threshold = 90;       // anything *above* this value will be considered BRIGHT
-                          // the range is 0-99, so by specifyinng 99, you only accept
-                          // 100% brightness. Note that a value of 100 would never be reached
-                          // because 100 is the max possible value, and it can never be
-                          // greater than 100.
+// the range is 0-99, so by specifyinng 99, you only accept
+// 100% brightness. Note that a value of 100 would never be reached
+// because 100 is the max possible value, and it can never be
+// greater than 100.
 unsigned long  minInterval = 3;  // minimum length of a pattern interval in millisec
-                          // this is a safety measure for inconsistencies. Only when a pattern change 
-                          // occurs that is at minimum of minInterval length will this be considered
-                          // a true pattern change
+// this is a safety measure for inconsistencies. Only when a pattern change
+// occurs that is at minimum of minInterval length will this be considered
+// a true pattern change
 bool active = false;      // currently, the sensor is receiving bright light
 bool isBright = false;    // when true, the sensor input is greater than 95% so there is strong light shining on it
 long  currentClock = 0;   // the milliseconds recorded at last pattern change
 long  duration = 0;       // duration of last pattern
-int   count = 0;
+int   count = 0;          // number of readings
 
 void setup() {
   Serial.begin(115200);
@@ -36,32 +36,35 @@ void loop() {
 
   // determine whether we currently detect a "dark" or "light" moment:
   isBright = value > threshold;
-  
+
   // do we have a status change (dark to light or vice versa)?
- if (isBright != active)
+  if (isBright != active)
   {
-   
-    
-     // how many millisecs did the last period take?
-     // we have a pattern change
-     duration = millis() - currentClock;
 
-     if (duration > minInterval)
-     {
-       currentClock = millis();             // reset the clock 
-       active = isBright;                   // reset the current mode 
 
-       // mark "dark" phases by negating the value
-       if (!isBright) { duration = duration * -1.0; }
-        
-       // recorded (previous) pattern was LIGHTED:
-       Serial.print("[");
-       Serial.print(duration);
-       Serial.print("]");
-       count++;
-     }
+    // how many millisecs did the last period take?
+    // we have a pattern change
+    duration = millis() - currentClock;
+
+    if (duration > minInterval)
+    {
+      currentClock = millis();             // reset the clock
+      active = isBright;                   // reset the current mode
+
+      // mark "dark" phases by negating the value
+      if (!isBright) {
+        duration = duration * -1.0;
+      }
+
+      // recorded (previous) pattern was LIGHTED:
+      Serial.print("[");
+      Serial.print(duration);
+      Serial.print("]");
+      count++;
+    }
   }
 
+  // add a new line after 10 readings:
   if (count > 10)
   {
     count = 0;
