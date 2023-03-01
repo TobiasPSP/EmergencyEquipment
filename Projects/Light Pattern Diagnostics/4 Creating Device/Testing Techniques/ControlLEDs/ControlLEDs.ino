5aeb368647b506ad430fabe524a6a7496b6a4a17
Dr. Tobias Weltner <tobias@powershell.one>
2023-03-01 18:00:59 +0100

    Reorg

---
+++ Projects/Light Pattern Diagnostics/4 Creating Device/Testing Techniques/ControlLEDs/ControlLEDs.ino
@@ -0,0 +1,15 @@
+
+
+#define LedLightDetect 11
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LedLightDetect, OUTPUT);
+  digitalWrite(LedLightDetect, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
